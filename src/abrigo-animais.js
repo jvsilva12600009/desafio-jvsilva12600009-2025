04cfb2d028b31e500960ed03ae65240f5b2f9b70
joaovictoresilva123 <joaovictor1235678910@gmail.com>
2025-09-08 11:45:44 -0300

    commit

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -23,19 +23,23 @@ class AbrigoAnimais {
       .filter(Boolean);
 
     
-    if (temDuplicados(p1) || temDuplicados(p2)) {
+    if (temDuplicados(p1) || temDuplicados(p2)) 
+    {
       return { erro: "brinquedo inválido" };
     }
 
-    if (!validarItens(p1, validBrinquedos) || !validarItens(p2, validBrinquedos)) {
+    if (!validarItens(p1, validBrinquedos) || !validarItens(p2, validBrinquedos)) 
+    {
       return { erro: "brinquedo inválido" };
     }
 
-    if (temDuplicados(ordem)) {
+    if (temDuplicados(ordem)) 
+    {
       return { erro: "Animal inválido" };
     }
 
-    if (!validarItens(ordem, nomesAnimais)) {
+    if (!validarItens(ordem, nomesAnimais)) 
+    {
       return { erro: "Animal inválido" };
     }
 
@@ -47,18 +51,22 @@ class AbrigoAnimais {
       const adotaP2 = podeAdotar(animal, p2);
 
 
-      if (adotaP1 && adotaP2) {
+      if (adotaP1 && adotaP2) 
+        {
         
-        if (adotadosP1 < 3 && adotadosP2 < 3) {
+        if (adotadosP1 < 3 && adotadosP2 < 3) 
+        {
           lista.push(`${animal} - abrigo`);
           continue;
-        }ga
-        if (adotadosP1 < 3 && adotadosP2 >= 3) {
+        }
+        if (adotadosP1 < 3 && adotadosP2 >= 3) 
+        {
           lista.push(`${animal} - pessoa 1`);
           adotadosP1++;
           continue;
         }
-        if (adotadosP2 < 3 && adotadosP1 >= 3) {
+        if (adotadosP2 < 3 && adotadosP1 >= 3) 
+        {
           lista.push(`${animal} - pessoa 2`);
           adotadosP2++;
           continue;
@@ -67,20 +75,27 @@ class AbrigoAnimais {
         lista.push(`${animal} - abrigo`);
         continue;
       }
-      if (adotaP1) {
-        if (adotadosP1 < 3) {
+      if (adotaP1) 
+      {
+        if (adotadosP1 < 3) 
+        {
           lista.push(`${animal} - pessoa 1`);
           adotadosP1++;
-        } else {
+        } 
+        else 
+        {
           lista.push(`${animal} - abrigo`);
         }
         continue;
       }
       if (adotaP2) {
-        if (adotadosP2 < 3) {
+        if (adotadosP2 < 3) 
+        {
           lista.push(`${animal} - pessoa 2`);
           adotadosP2++;
-        } else {
+        } 
+        else 
+        {
           lista.push(`${animal} - abrigo`);
         }
         continue;
@@ -91,8 +106,14 @@ class AbrigoAnimais {
     lista.sort((a, b) => {
       const nomeA = a.split(" - ")[0].toLowerCase();
       const nomeB = b.split(" - ")[0].toLowerCase();
-      if (nomeA < nomeB) return -1;
-      if (nomeA > nomeB) return 1;
+      if (nomeA < nomeB) 
+        {
+        return -1;
+      }
+      if (nomeA > nomeB)
+        {
+        return 1;
+      } 
       return 0;
     });
 
